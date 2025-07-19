7d7f97a7edf23b88312eb6dee37431210111f718
DuowngTora Mac <nvduong216@gmail.com Mac>
2025-07-20 00:19:30 +0700

    ADD - add/chat-memory: Add memory to chat method

--- src/main/java/com/duowngtora/spring_ai_gemini/controller/ChatMessageController.java
+++ src/main/java/com/duowngtora/spring_ai_gemini/controller/ChatMessageController.java
@@ -2,9 +2,12 @@ package com.duowngtora.spring_ai_gemini.controller;
 
 import com.duowngtora.spring_ai_gemini.service.IChatService;
 import com.duowngtora.spring_ai_gemini.service.dto.request.ChatMessageReqDto;
+import com.duowngtora.spring_ai_gemini.service.dto.response.ChatMessageResDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -17,13 +20,14 @@ public class ChatMessageController {
     }
 
     @PostMapping("/chat-message")
-    public ResponseEntity<String> chatMessage(@RequestBody ChatMessageReqDto chatMessageReqDto){
+    public ResponseEntity<ChatMessageResDto> chatMessage(@RequestBody ChatMessageReqDto chatMessageReqDto){
         return ResponseEntity.ok().body(this.chatService.chatMessage(chatMessageReqDto));
     }
 
     @PostMapping("/chat-with-file")
-    public ResponseEntity<String> chatWithFile(@RequestParam("file") MultipartFile file,
-                                               @RequestParam("message") String message){
-        return ResponseEntity.ok().body(this.chatService.chatWithFile(file, message));
+    public ResponseEntity<ChatMessageResDto> chatWithFile(@RequestParam("file") MultipartFile file,
+                                               @RequestParam("message") String message,
+                                               @RequestParam(value = "conversationId", required = false) UUID conversationId){
+        return ResponseEntity.ok().body(this.chatService.chatWithFile(file, message, conversationId));
     }
 }
--- src/main/java/com/duowngtora/spring_ai_gemini/service/impl/ChatServiceImpl.java
+++ src/main/java/com/duowngtora/spring_ai_gemini/service/impl/ChatServiceImpl.java
@@ -2,9 +2,14 @@ package com.duowngtora.spring_ai_gemini.service.impl;
 
 import com.duowngtora.spring_ai_gemini.service.IChatService;
 import com.duowngtora.spring_ai_gemini.service.dto.request.ChatMessageReqDto;
+import com.duowngtora.spring_ai_gemini.service.dto.response.ChatMessageResDto;
 import com.duowngtora.spring_ai_gemini.service.dto.response.ExpenseInfoResDto;
 import com.duowngtora.spring_ai_gemini.service.dto.response.FilmInfoResDto;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.ChatOptions;
@@ -17,31 +22,55 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class ChatServiceImpl implements IChatService {
 
     private final ChatClient chatClient;
 
-    public ChatServiceImpl(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    private final JdbcChatMemoryRepository jdbcChatMemoryRepository;
+
+    public ChatServiceImpl(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        this.jdbcChatMemoryRepository = jdbcChatMemoryRepository;
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(30)
+                .build();
+
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @Override
-    public String chatMessage(ChatMessageReqDto chatMessageReqDto) {
+    public ChatMessageResDto chatMessage(ChatMessageReqDto chatMessageReqDto) {
         SystemMessage systemMessage = new SystemMessage("""
                     Bạn là DuowngTora, một chuyên viên chuyên xử lý những thủ tục hành chính công của Cục An Toàn Thực Phẩm
                     Bộ Y tế. Bạn có thể trả lời bằng tiếng Việt hoặc tiếng Anh tùy thuộc vào yêu cầu người dùng.
                     Hãy cố gắng trả lời ngắn gọn và rõ ràng nhất có thể để giúp đỡ người dùng tốt hơn.
                     Nếu bạn không chắc chắn về câu hỏi của người dùng, hãy nói rằng bạn sẽ tìm hiểu thêm trước khi trả lời.
                 """);
+
+        UUID conversationId;
+        if(chatMessageReqDto.conversationId() == null)
+            conversationId = UUID.randomUUID();
+        else {
+            conversationId = chatMessageReqDto.conversationId();
+        }
+
         UserMessage userMessage = new UserMessage(chatMessageReqDto.message());
         Prompt prompt = new Prompt(systemMessage, userMessage);
-        return this.chatClient.prompt(prompt).call().content();
+        return new ChatMessageResDto(this.chatClient.prompt(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .call().content(),
+            conversationId
+        );
     }
 
     @Override
-    public String chatWithFile(MultipartFile file, String message) {
+    public ChatMessageResDto chatWithFile(MultipartFile file, String message, UUID conversationId) {
         Media media = Media.builder()
                 .mimeType(MimeTypeUtils.parseMimeType(Objects.requireNonNull(file.getContentType())))
                 .data(file.getResource())
@@ -52,15 +81,23 @@ public class ChatServiceImpl implements IChatService {
                 .temperature(0.1D)
                 .build();
 
-        return this.chatClient.prompt()
+
+        if(conversationId == null)
+            conversationId = UUID.randomUUID();
+        UUID finalConversationId = conversationId;
+
+        return new ChatMessageResDto(this.chatClient.prompt()
                 .options(chatOptions)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, finalConversationId.toString()))
                 .system("""
                         Bạn là DuowngTora
                         """)
                 .user(promptUserSpec ->
                         promptUserSpec.media(media)
                                 .text(message))
-                .call().content();
+                .call().content(),
+                finalConversationId
+        );
     }
 
     @Override
@@ -75,7 +112,15 @@ public class ChatServiceImpl implements IChatService {
 
         Prompt prompt = new Prompt(systemMessage, userMessage);
 
+        UUID conversationId;
+        if(chatMessageReqDto.conversationId() == null)
+            conversationId = UUID.randomUUID();
+        else {
+            conversationId = chatMessageReqDto.conversationId();
+        }
+
         return this.chatClient.prompt(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .options(chatOptions)
                 .call()
                 .entity(new ParameterizedTypeReference<List<FilmInfoResDto>>() {
@@ -92,7 +137,16 @@ public class ChatServiceImpl implements IChatService {
         ChatOptions chatOptions = ChatOptions.builder()
                 .temperature(0.1D)
                 .build();
+
+        UUID conversationId;
+        if(chatMessageReqDto.conversationId() == null)
+            conversationId = UUID.randomUUID();
+        else {
+            conversationId = chatMessageReqDto.conversationId();
+        }
+
         return this.chatClient.prompt(prompt)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .options(chatOptions)
                 .call()
                 .entity(ExpenseInfoResDto.class);
